83ab3b908c83ea0b81275fc5aa07ee999c2f67f0
GDEV <developer.nergis.umut11@gmail.com>
2026-09-13 18:39:54 +0300

    Remove alert from GestaltEditApp

--- GestaltEdit/GestaltEditApp.swift
+++ GestaltEdit/GestaltEditApp.swift
@@ -13,25 +13,5 @@ struct GestaltEditApp: App {
     init() {
         AutomationCommand.runIfNeeded()
     }
-
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-                .environmentObject(viewModel)
-                .alert("GestaltEdit", isPresented: Binding(
-                    get: { !hasShownVersionNotice },
-                    set: { _ in hasShownVersionNotice = true }
-                )) {
-                    Button("Tamam") {
-                        hasShownVersionNotice = true
-                    }
-                } message: {
-                    Text("""
-                   This App designed İOS/İpadOS 27 .
-
-                    Some Gestalt fields may not be available depending on the iOS version.
-                    """)
-                }
-        }
     }
 }
